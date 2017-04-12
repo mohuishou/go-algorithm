bdb2bf8f5811ebfe70c6fd16af26f08030818c7d
mohuishou <1@lailin.xyz>
2017-04-12 10:25:29 +0800

    update

--- Graph/Graph_test.go
+++ Graph/Graph_test.go
@@ -1,18 +1,18 @@
 package Graph
 
 import (
-	"fmt"
-	"testing"
+    "fmt"
+    "testing"
 )
 
 func TestGraph(t *testing.T) {
-	graph := BuildGraph("test.txt")
-	fmt.Println(graph)
-	e := graph[1].FisrtEdge
-	for e != nil {
-		if e.v != 2 || e.weight != 3 {
-			t.Fatal("错误：建图失败，e.v!=2,e.weight!=3")
-		}
-		e = e.next
-	}
+    graph := BuildGraph("test.txt")
+    fmt.Println(graph)
+    e := graph[1].FisrtEdge
+    for e != nil {
+        if e.v != 2 || e.weight != 3 {
+            t.Fatal("错误：建图失败，e.v!=2,e.weight!=3")
+        }
+        e = e.next
+    }
 }
